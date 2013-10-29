cb6c96012b631eae263e55805b44858a850e1399
hjing <hjing@walmartlabs.com>
2013-10-29 16:47:11 -0700

    first commit

---
+++ loader.rb
@@ -0,0 +1,51 @@
+@@filename = ARGV[0]
+@@modelname = ARGV[1]
+@@Database = 'NRT-dev'
+
+def headerFetcher(filename)
+  headers = []
+  f = File.open(filename,'r')
+  f.each_with_index do |l, i|
+    break if i > 0
+    headers = l.split("\t")
+    headers[-1] = headers[-1][0..-2]
+    print headers
+    puts
+
+  end
+  f.close
+  return headers
+end
+
+def modelGen(modelname)
+  headers =  headerFetcher(@@filename)
+  result = headers.join(":text ")
+  result = "rails g model #{modelname} #{result}:text"
+  puts result
+end
+
+# Load products
+def loadProducts
+  tablename = @@modelname.downcase + 's'
+  puts tablename
+  headers =  headerFetcher(@@filename)
+  column_names = headers.join(",")
+  f = File.open(@@filename)
+  o = File.open("#{tablename}.csv","w")
+  f.each do |l|
+    o.write(l)
+  end
+  f.close
+  o.close
+  cmd_list = []
+  cmd_list << "mysqlimport --columns='#{column_names}' --ignore-lines=1 --fields-terminated-by='\t' --local -u root -p #{@@Database} #{tablename}.csv"
+  cmd_list << "rm #{tablename}.csv"
+  cmd_list.each do |c|
+    `#{c}`
+  end
+end
+
+# ruby loader.rb nicotine_products.tsv Product
+modelGen(@@modelname)
+loadProducts
+
