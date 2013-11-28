45af87c325d175b146f38a49570a9ed8a0e27f5f
Han Jing <hanqijing@gmail.com>
2013-11-27 20:15:24 -0800

    more refractoring

--- loader.rb
+++ loader.rb
@@ -2,21 +2,19 @@ require 'trollop'
 require_relative 'loader_helper'
 
 opts = Trollop::options do
-  opt :database, "Database Name", :type => :string                    # flag --monkey, default false
-  opt :table, "Table Name", :type => :string        # string --name <s>, default nil
-  opt :filename, "File Name", :type => :string        # string --name <s>, default nil
-  opt :username, "User Name", :type => :string        # string --name <s>, default nil
-  opt :model, "Model Name", :type => :string        # string --name <s>, default nil
+  opt :database, "Database Name", :type => :string             
+  opt :table, "Table Name", :type => :string        
+  opt :filename, "File Name", :type => :string     
+  opt :username, "User Name", :type => :string    
+  opt :model, "Model Name", :type => :string     
   opt :verbose, "Verbose Mode"
   opt :dryrun, "No operation. Just print out the shell commands."
 end
 
-p opts # a hash: { :monkey=>false, :name=>nil, :num_limbs=>4, :help=>false }
+#p opts # a hash: { :monkey=>false, :name=>nil, :num_limbs=>4, :help=>false }
 
-# ruby loader.rb nicotine_products.tsv Product
+# ruby loader.rb --file product_dump.tsv --table test_table --database test_db
 #modelGen(@modelname)
-#loadProducts
-#headerFetcher(@Filename)
 loader = LoaderHelper.new(
                            file=opts[:filename], 
                            modelname = opts[:model], 
